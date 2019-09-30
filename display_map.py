2531e03c2e55cd22dde851263613a5664285fcac
jallen <35147824+ShakesLabs@users.noreply.github.com>
2019-09-30 16:36:05 -0600

    Got the python file started

---
+++ display_map.py
@@ -0,0 +1,21 @@
+# display_map.py
+# We want to print out basic text representation of a map
+
+print("XXX")
+print("XYX")
+print("XXX")
+
+new_variable = "I really like to " 
+var2 = "pet fat dogs. " 
+new_variable = new_variable + var2
+print(new_variable)
+
+
+var3 = 213 
+var4 = 15.5
+print(var3 + var4)
+print(var3)
+
+int(var4)
+print(var4)
+
