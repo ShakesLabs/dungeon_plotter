08e6ec2695e9c0d45cbcad6a056f2c2107aa6693
CurieShakes <35147824+ShakesLabs@users.noreply.github.com>
2019-10-01 13:18:28 -0600

    cleaned up practice stuff in preparation for map generation code

--- display_map.py
+++ display_map.py
@@ -1,25 +1,7 @@
 # display_map.py
-# We want to print out basic text representation of a map
-# to do: assign dungeon objects to variables representing their "ness" 
-# and present them. randomly select variables to fill a 5x5 square.
+# Print out basic text representation of a map
+# to do: assign variables representing map features 
+# randomly select variables to fill a 5x5 square
 
 tree = "T"
-
-print("XXX")
-print("XYX")
-print("XXX")
-
-new_variable = "I really like to " 
-var2 = "pet fat dogs. " 
-new_variable = new_variable + var2
-print(new_variable)
-
-
-var3 = 213 
-var4 = 15.5
-print(var3 + var4)
-print(var3)
-
-int(var4)
-print(var4)
 
