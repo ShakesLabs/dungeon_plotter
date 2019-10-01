76de7e66beda171c56bb624ec43a434ecc84e54f
Lil Debbie <sarahdeborahallen@gmail.com>
2019-10-01 16:14:38 -0500

    added loops

--- display_map.py
+++ display_map.py
@@ -3,5 +3,28 @@
 # to do: assign variables representing map features 
 # randomly select variables to fill a 5x5 square
 
-tree = "T"
+import random
 
+tree = "T"
+water = "~"
+path = "="
+monster = "!"
+wall = "I"
+
+i = 1
+while i < 6:
+    tile = random.randint(1, 5)
+    if tile == 1: 
+        print(tree)
+    elif tile == 2: 
+        print(water)
+    elif tile == 3: 
+        print(path)
+    elif tile == 4:
+        print(monster)
+    elif tile == 5:
+        print(wall)
+    else:
+        print("error")
+    i += 1
+    
